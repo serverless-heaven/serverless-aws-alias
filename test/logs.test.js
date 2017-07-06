48375b52caa84f86c6a38cb69a7d6eb584731941
Frank Schmid <fschmid@stashimi.com>
2017-07-06 15:20:27 +0200

    Added unit tests for logs

--- test/logs.test.js
+++ test/logs.test.js
@@ -20,28 +20,30 @@ describe('logs', () => {
 	let awsAlias;
 	// Sinon and stubs for SLS CF access
 	let sandbox;
-	//let providerRequestStub;
+	let providerRequestStub;
 	let logStub;
+	let aliasGetAliasFunctionVersionsStub;
 
 	before(() => {
 		sandbox = sinon.sandbox.create();
 	});
 
 	beforeEach(() => {
-		serverless = new Serverless();
 		options = {
 			alias: 'myAlias',
 			stage: 'dev',
 			region: 'us-east-1',
 			function: 'first'
 		};
-		serverless.setProvider('aws', new AwsProvider(serverless));
+		serverless = new Serverless(options);
+		serverless.setProvider('aws', new AwsProvider(serverless, options));
 		serverless.cli = new serverless.classes.CLI(serverless);
 		serverless.service.service = 'testService';
 		serverless.service.provider.compiledCloudFormationAliasTemplate = {};
 		awsAlias = new AWSAlias(serverless, options);
-		//providerRequestStub = sandbox.stub(awsAlias._provider, 'request');
+		providerRequestStub = sandbox.stub(awsAlias._provider, 'request');
 		logStub = sandbox.stub(serverless.cli, 'log');
+		aliasGetAliasFunctionVersionsStub = sandbox.stub(awsAlias, 'aliasGetAliasFunctionVersions');
 
 		logStub.returns();
 	});
@@ -103,25 +105,39 @@ describe('logs', () => {
 			};
 		});
 
-		/** TODO: Use fake alias log stream responses here!
-		it('should get log streams with correct params', () => {
-			const replyMock = {
+		it('should get log streams', () => {
+			const streamReply = {
 				logStreams: [
 					{
 						logStreamName: '2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
 						creationTime: 1469687512311,
 					},
 					{
-						logStreamName: '2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
+						logStreamName: '2016/07/28/[20]BE6A2C395AA244C8B7069D8C48B03B9E',
+						creationTime: 1469687512311,
+					},
+					{
+						logStreamName: '2016/07/28/[20]83f5206ab2a8488290349b9c1fbfe2ba',
+						creationTime: 1469687512311,
+					},
+					{
+						logStreamName: '2016/07/28/[10]83f5206ab2a8488290349b9c1fbfe2ba',
 						creationTime: 1469687512311,
 					},
 				],
 			};
-			providerRequestStub.resolves(replyMock);
+			providerRequestStub.resolves(streamReply);
+			aliasGetAliasFunctionVersionsStub.returns(BbPromise.resolve([
+				{
+					functionName: 'func1',
+					functionVersion: '20'
+				}
+			]));
+			awsAlias._lambdaName = 'func1';
 
 			return expect(awsAlias.logsGetLogStreams()).to.be.fulfilled
 			.then(logStreamNames => BbPromise.all([
-				expect(providerRequestStub).to.have.been.calledTwice,
+				expect(providerRequestStub).to.have.been.calledOnce,
 				expect(providerRequestStub).to.have.been.calledWithExactly(
 					'CloudWatchLogs',
 					'describeLogStreams',
@@ -134,15 +150,18 @@ describe('logs', () => {
 					awsAlias.options.stage,
 					awsAlias.options.region
 				),
+				expect(logStreamNames).to.have.lengthOf(2),
 				expect(logStreamNames[0])
-					.to.be.equal('2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba'),
+					.to.be.equal('2016/07/28/[20]BE6A2C395AA244C8B7069D8C48B03B9E'),
 				expect(logStreamNames[1])
-					.to.be.equal('2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba'),
+					.to.be.equal('2016/07/28/[20]83f5206ab2a8488290349b9c1fbfe2ba'),
 			]));
 		});
 
 		it('should throw error if no log streams found', () => {
 			providerRequestStub.resolves();
+			aliasGetAliasFunctionVersionsStub.returns(BbPromise.resolve([]));
+
 			return expect(awsAlias.logsGetLogStreams()).to.be.rejectedWith("");
 		});
 	});
@@ -164,20 +183,20 @@ describe('logs', () => {
 			const replyMock = {
 				events: [
 					{
-						logStreamName: '2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
+						logStreamName: '2016/07/28/[20]BE6A2C395AA244C8B7069D8C48B03B9E',
 						timestamp: 1469687512311,
 						message: 'test',
 					},
 					{
-						logStreamName: '2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
+						logStreamName: '2016/07/28/[20]BE6A2C395AA244C8B7069D8C48B03B9E',
 						timestamp: 1469687512311,
 						message: 'test',
 					},
 				],
 			};
 			const logStreamNamesMock = [
-				'2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
-				'2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
+				'2016/07/28/[20]BE6A2C395AA244C8B7069D8C48B03B9E',
+				'2016/07/28/[20]83f5206ab2a8488290349b9c1fbfe2ba',
 			];
 			providerRequestStub.resolves(replyMock);
 			awsAlias.serverless.service.service = 'new-service';
@@ -188,6 +207,7 @@ describe('logs', () => {
 				logGroupName: awsAlias.provider.naming.getLogGroupName('new-service-dev-first'),
 				startTime: '3h',
 				filter: 'error',
+				alias: 'myAlias',
 			};
 
 			return expect(awsAlias.logsShowLogs(logStreamNamesMock)).to.be.fulfilled
@@ -213,52 +233,50 @@ describe('logs', () => {
 			const replyMock = {
 				events: [
 					{
-						logStreamName: '2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
+						logStreamName: '2016/07/28/[20]BE6A2C395AA244C8B7069D8C48B03B9E',
 						timestamp: 1469687512311,
 						message: 'test',
 					},
 					{
-						logStreamName: '2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
+						logStreamName: '2016/07/28/[20]BE6A2C395AA244C8B7069D8C48B03B9E',
 						timestamp: 1469687512311,
 						message: 'test',
 					},
 				],
 			};
 			const logStreamNamesMock = [
-				'2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
-				'2016/07/28/[$LATEST]83f5206ab2a8488290349b9c1fbfe2ba',
+				'2016/07/28/[20]BE6A2C395AA244C8B7069D8C48B03B9E',
+				'2016/07/28/[20]83f5206ab2a8488290349b9c1fbfe2ba',
 			];
-			const filterLogEventsStub = sinon.stub(awsLogs.provider, 'request').resolves(replyMock);
-			awsLogs.serverless.service.service = 'new-service';
-			awsLogs.options = {
+			providerRequestStub.resolves(replyMock);
+			awsAlias.serverless.service.service = 'new-service';
+			awsAlias._options = {
 				stage: 'dev',
 				region: 'us-east-1',
-				function: 'first',
-				logGroupName: awsLogs.provider.naming.getLogGroupName('new-service-dev-first'),
+				function: 'func1',
+				logGroupName: awsAlias.provider.naming.getLogGroupName('new-service-dev-func1'),
 				startTime: '2010-10-20',
 				filter: 'error',
+				alias: 'myAlias',
 			};
 
-			return awsLogs.showLogs(logStreamNamesMock)
-				.then(() => {
-					expect(filterLogEventsStub.calledOnce).to.be.equal(true);
-					expect(filterLogEventsStub.calledWithExactly(
+			return expect(awsAlias.logsShowLogs(logStreamNamesMock)).to.be.fulfilled
+				.then(() => BbPromise.all([
+					expect(providerRequestStub).to.have.been.calledOnce,
+					expect(providerRequestStub).to.have.been.calledWithExactly(
 						'CloudWatchLogs',
 						'filterLogEvents',
 						{
-							logGroupName: awsLogs.provider.naming.getLogGroupName('new-service-dev-first'),
+							logGroupName: awsAlias.provider.naming.getLogGroupName('new-service-dev-func1'),
 							interleaved: true,
 							logStreamNames: logStreamNamesMock,
-							startTime: 1287532800000, // '2010-10-20'
+							startTime: 1287532800000,
 							filterPattern: 'error',
 						},
-						awsLogs.options.stage,
-						awsLogs.options.region
-					)).to.be.equal(true);
-
-					awsLogs.provider.request.restore();
-				});
+						awsAlias.options.stage,
+						awsAlias.options.region
+					),
+				]));
 		});
-		*/
 	});
 });
